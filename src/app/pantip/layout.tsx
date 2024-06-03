e63baca1e028605f3336ee0a0ccfc7225245e2dc
NattSass67 <nattanun.sk@gmail.com>
2024-06-03 22:13:10 +0700

    fixed-footer

--- src/app/pantip/layout.tsx
+++ src/app/pantip/layout.tsx
@@ -1,32 +1,25 @@
-'use client';
+'use client'
 
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { useAppDispatch } from '@/session/store';
-import { fetchContent } from '@/session/my-state';
-import { useEffect } from 'react';
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { useAppDispatch } from '@/session/store'
+import { fetchContent } from '@/session/my-state'
+import { useEffect } from 'react'
 
-import { persistor, store } from '@/session/store';
+import { persistor, store } from '@/session/store'
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
   useEffect(() => {
-    dispatch(fetchContent());
+    dispatch(fetchContent())
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <div className="">{children}</div>
-      </PersistGate>
-    </Provider>
-  );
+  }, [])
+  return <div className="">{children}</div>
 }
-
 // Enable edge runtime but you are required to disable the `migrate` function in `src/libs/DB.ts`
 // Unfortunately, this also means it will also disable the automatic migration of the database
 // And, you will have to manually migrate it with `drizzle-kit push`
